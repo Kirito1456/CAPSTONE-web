c8598d9f68bb8c6e52f865888486ab7ecf8b2382
Dell <winnie_s_he@dlsu.edu.ph>
2024-03-08 15:29:50 +0800

    css

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'daphne',
     'django.contrib.staticfiles',
-    'channels',
 ]
 
 MIDDLEWARE = [
